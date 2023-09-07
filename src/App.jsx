e7301531cf8224b4f76c08e4d747c82695ad77bb
costiy <tudorconstantin1405@gmail.com>
2023-09-07 17:05:06 +0300

    Am schimbat numele site-ului si am adaugat o mica componenta in functia App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+const Salut = () => {
+  return (<h1>Site cu poze MEME</h1>);
+};
+
 function App() {
-  return null;
-}
+  return (<center><div>
+    <Salut/>
+  </div>
+  </center>)
+};
 
 export default App
 
