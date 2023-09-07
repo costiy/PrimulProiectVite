5846b6696c96fccb44b3a68dafc7225d2de474d3
costiy <tudorconstantin1405@gmail.com>
2023-09-07 17:21:28 +0300

    Am facut prima componenta

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-const Salut = () => {
+const Titlu = () => {
   return (<div>
     <h1>Site cu poze MEME</h1>
     <h2>Aici voi pune cateva poze MEME si voi exersa pentru curs</h2>
     </div>);
 };
 
+const Poze = () => {
+  return (<div>
+  <img src = "fortnutavatar.png"></img>
+  <img src = "HappyCAT.jpg"></img>
+  <img src = "LamaGermanaAVATAR.jpg"></img>
+  <img src = "PASTOASOMUIE.png"></img>
+  </div>)
+};
+
 function App() {
   return (<center><div>
-    <Salut/>
+    <Titlu/>
+    <Poze/>
   </div>
   </center>)
 };
